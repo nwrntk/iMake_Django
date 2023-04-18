061ed4558080291c830ef5e8b19dfd02d451e9f2
nwrntk <fernwrnthaksm@gmail.com>
2023-04-18 14:34:25 +0700

    clean opencv code

--- backendapp/face_detector/views.py
+++ backendapp/face_detector/views.py
@@ -7,6 +7,9 @@ import json
 import cv2
 import os
 import datetime
+import base64
+
+from time import process_time
 
 from django.shortcuts import render, HttpResponse
 
@@ -14,71 +17,63 @@ from django.shortcuts import render, HttpResponse
 def hello_django(request):
     return HttpResponse("Hello Django!")
 
+def date_time():
+    x = datetime.datetime.now()
+    return x.strftime("%d %b %Y %X")
 
 # define the path to the face detector
 FACE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_frontalface_default.xml".format(
-	base_path=os.path.abspath(os.path.dirname(__file__)))
+ base_path=os.path.abspath(os.path.dirname(__file__)))
 @csrf_exempt
 def detect(request):
+    minneighbors =  request.POST.get("minneighbors",None)
     
-	# initialize the data dictionary to be returned by the request
-	data = {"success": False}
-	# check to see if this is a post request
-	if request.method == "POST":
-		# check to see if an image was uploaded
-		if request.FILES.get("image", None) is not None:
-			# grab the uploaded image
-			image = _grab_image(stream=request.FILES["image"])
-		# otherwise, assume that a URL was passed in
-		else:
-			# grab the URL from the request
-			url = request.POST.get("url", None)
-			# if the URL is None, then return an error
-			if url is None:
-				data["error"] = "No URL provided."
-				return JsonResponse(data)
-			# load the image and convert
-			image = _grab_image(url=url)
-		# convert the image to grayscale, load the face cascade detector,
-		# and detect faces in the image
-		# global op_img
-		# op_img = image
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
-		rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=8,
-			minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
-		
-  		# construct a list of bounding boxes from the detection
-		rects = [(int(x), int(y), int(x + w), int(y + h)) for (x, y, w, h) in rects]
-		
-		for (startX, startY, endX, endY) in rects:
-			cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+    t_start = process_time()
+    # initialize the data dictionary to be returned by the request
+    data = {"success": False}
+    # check to see if this is a post request
+    if request.method == "POST":
+        # grab the URL from the request
+        url = request.POST.get("url", None)
+        # if the URL is None, then return an error
+        if url is None:
+            data["error"] = "No URL provided."
+            return JsonResponse(data)
+        # load the image and convert
+        image = _grab_image(url=url)
+        
+        # convert the image to grayscale, load the face cascade detector,
+        # and detect faces in the image
+        # global op_img
+        # op_img = image
+        dt = date_time()
+        colorImg = image.copy()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
+        rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=int(minneighbors), minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+        # construct a list of bounding boxes from the detection
+        rects = [(int(x), int(y), int(x + w), int(y + h)) for (x, y, w, h) in rects]
+        for (startX, startY, endX, endY) in rects:
+            cv2.rectangle(colorImg, (startX, startY), (endX, endY), (0, 255, 0), 2)
+        
+        ret, buffer = cv2.imencode('.jpg', colorImg)
+        jpg_as_text = base64.b64encode(buffer)
 
-  		# update the data dictionary with the faces detected
-		data.update({"image": len(rects), "face": rects, "date": True, "pro_time": True, "success": True})
-		# data.update({"image matrix": image, "num_faces": len(rects), "faces": rects, "success": True})
-	
-	# return a JSON response
-	return JsonResponse(data)
-	# return JsonResponse({"image" : op_img, "success": True})
+        t_stop = process_time()
+        total = t_stop-t_start
+        # update the data dictionary with the faces detected
+        data.update({"image": str(jpg_as_text)[2:-1], "face": len(rects), "date": str(dt), "pro_time": str(total), "success": True})
+    
+    # return a JSON response
+    return JsonResponse(data)
 
-def _grab_image(path=None, stream=None, url=None):
-	# if the path is not None, then load the image from disk
-	if path is not None:
-		image = cv2.imread(path)
-	# otherwise, the image does not reside on disk
-	else:	
-		# if the URL is not None, then download the image
-		if url is not None:
-			resp = urllib.request.urlopen(url)
-			data = resp.read()
-		# if the stream is not None, then the image has been uploaded
-		elif stream is not None:
-			data = stream.read()
-		# convert the image to a NumPy array and then read it into
-		# OpenCV format
-		image = np.asarray(bytearray(data), dtype="uint8")
-		image = cv2.imdecode(image, cv2.IMREAD_COLOR)
- 
-	# return the image
-	return image
+
+def _grab_image(url=None):
+    if url is not None:
+        resp = urllib.request.urlopen(url)
+        data = resp.read()
+        
+    image = np.asarray(bytearray(data), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    
+    return image
